229e8594432100f384cee944ff337b79a003ecbd
GabrielRosaS2 <gabriel030203.gb@gmail.com>
2025-08-26 22:58:07 -0300

    Criação da pagina de configuração. Deixar funcional

--- src/routes/app/settings.tsx
+++ src/routes/app/settings.tsx
@@ -1,3 +1,30 @@
+import './settingsStyle.css'
+
 export function SettingsPage() {
-    return <div className="p-6">Settings page coming soon...</div>;
+    return (
+    <div className="settingsPage">
+        <h1 className="TitleSettings"> 
+            CONFIGURAÇÕES
+        </h1>
+        <div className="user">
+            <h2 className="dashboardsFavoritos">
+                MODELO DE DASHBOARD FAVORITOS
+                <p> Escolha seu Dashboard Favorito </p>
+            </h2>
+            <h2 className="vozDeLeitura">
+                VOZ DE LEITURA 
+                <p> Ative sua Voz de Leitura</p>
+            </h2>
+            <h2 className="idiomaSettings">
+                IDIOMA
+                <p> Escolha seu Idioma </p>
+            </h2>
+            <h2 className="notificacoes">
+                NOTIFICAÇÕES
+                <p> Ativar as notificações</p>
+                <p> Ativar as notificações</p>
+            </h2>
+        </div>
+    </div>
+    )
 }
